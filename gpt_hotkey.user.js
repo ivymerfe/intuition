d4f99d02c1f91242d68424f37409c8a43b4da85b
ivymerfe <82306640+ivymerfe@users.noreply.github.com>
2025-04-19 22:58:29 +0700

    better hotkey

--- gpt_hotkey.user.js
+++ gpt_hotkey.user.js
@@ -5,42 +5,37 @@
 // @author       Ivy
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        GM_setClipboard
 // @run-at document-start
 // ==/UserScript==
 
-(function () {
-  "use strict";
+(function() {
+  'use strict';
   async function trySend(text) {
     await fetch("http://localhost:5000/send", {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ send: text }),
-      mode: "no-cors",
+      body: JSON.stringify({send: text}),
+      mode: "no-cors"
     });
   }
-
+  var selectedText = "";
+  document.addEventListener("selectionchange", (e) => {
+    const text = getSelection().toString();
+    if (text) {
+      selectedText = text;
+    }
+  });
   document.addEventListener("keyup", (e) => {
-    var handled = false;
-    if (e.key == "1" && e.altKey) {
-      handled = true;
-      var selectedText = getSelection().toString();
-      if (selectedText != "") {
-        trySend(selectedText);
-      } else if (e.target.value) {
-        trySend(e.target.value);
-      }
-    }
-    if (e.key == "2" && e.altKey && e.target.value) {
-      handled = true;
-      GM_setClipboard(e.target.value);
-    }
-    if (handled) {
+    if (e.key == '1' && e.altKey) {
       e.preventDefault();
       e.stopImmediatePropagation();
+      if (selectedText != "") {
+        trySend(selectedText);
+        selectedText = "";
+      }
     }
   });
 })();
